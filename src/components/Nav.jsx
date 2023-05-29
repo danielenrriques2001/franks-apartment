4f32ed3363ff276dbd8a0f4a222b1436028b73b7
daniel <daniel.enrriquesmercedes@gmail.com>
2023-05-29 13:48:45 +0200

    FT: i created the hamburgerbutton component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+
+const Nav = () => {
+  return (
+    <ul class="menu">
+        <li><a className="menuItem" href="/apartment">Apartment</a></li>
+        <li><a className="menuItem" href="/umgebung">Prenzlauer Berg</a></li>
+        <li><a className="menuItem" href="/kontakt">Kontakt</a></li>
+  </ul>
+  )
+}
+
+export default Nav
