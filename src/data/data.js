d95dd399125f88d2a0ee85d34dc837967da05268
daniel <daniel.enrriquesmercedes@gmail.com>
2023-07-06 21:50:28 +0200

    ft: I cahnged the hero and the gallery section

--- src/data/data.js
+++ src/data/data.js
@@ -3,26 +3,26 @@ export const arrayImages = ['franks-1', 'franks-2', 'franks-3', 'franks-4', 'fra
 
 export const heroContent = {
     de:{ 
-        hero: `Finde dein <br> <span>Zuhause</span> <br> in <br> unserem <br>  Kiez!`,
-        button: 'Jetzt Buchen'},
+        hero: `Finde dein <span>Zuhause</span> in unserem Kiez!`,
+        button: 'Verfügbarkeit prüfen'},
     en: {
-        hero: `Find your <br> <span>Home</span> <br> in <br> our <br>  Kiez!`,
-        button: 'Reserve Now'}
+        hero: `Find your <span>Home</span> in our Kiez!`,
+        button: 'Check Availability'}
 }
 
 export const descriptionContent = {
     de: {
-        heading: `Willkommen bei franks apartment im bunten und weltoffenen Kiez <span>Prenzlauer Berg.</span>`,
+        heading: `Willkommen bei frank’s apartment in <span>Prenzlauer Berg.</span>`,
 
-        description: `Auf 70m² findet Ihr ausreichend Platz für bis zu vier Personen. Das liebevoll eingerichtete Apartment ist komplett für Selbstversorger ausgestattet und hat allen Komfort. Für Euer Wohlbefinden bietet Euch Frank Massagen direkt im Haus an.`,
+        description: `Auf 70m² findet Ihr ausreichend Platz für bis zu vier Personen. Das liebevoll eingerichtete Apartment ist komplett für Selbstversorger ausgestattet und hat allen Komfort.`,
 
         massage: `Für Euer Wohlbefinden bietet Euch Frank <span><a href="/de/massage">Massagen</a></span> direkt im Haus an.`,
     },
 
     en: {
-        heading: `Welcome to frank's Apartment in the colourful and cosmopolitan neighbourhood <span>Prenzlauer Berg.</span>`,
+        heading: `Welcome to frank's Apartment in <span>Prenzlauer Berg.</span>`,
 
-        description: `On 70m² you will find enough space for up to four people. The lovingly furnished flat is fully equipped for self-catering and has all the comforts. For your well-being, Frank offers massages directly in the house.`,
+        description: `On 70m² you will find enough space for up to four people. The lovingly furnished flat is fully equipped for self-catering and has all the comforts.`,
         
         massage: `For your well-being, Frank offers you <span><a href="/en/massage">Massages</a></span> directly in the house.`,
     }
@@ -30,9 +30,9 @@ export const descriptionContent = {
 
 export const footerContent = {
     de: {
-        para: `Franks Apartment | Frank Hüllmandel | Telefon: +49 (030) 91704776 | E-Mail: info@franksberlin.com | <a href=''>Anfahrt</a> | <a href=''>Impressum</a>`,
+        para: `Franks Apartment | Frank Hüllmandel | Whatsapp: +49 (0) 177 299 5212 | E-Mail: info@franksberlin.com | <a href=''>Anfahrt</a> | <a href=''>Impressum</a>`,
     },
-    en: { para: `Franks Apartment | Frank Hüllmandel | Telephone: +49 (030) 91704776 | E-Mail: info@franksberlin.com | <a href=''>Directions</a> | <a href=''>Imprint</a>`},
+    en: { para: `Franks Apartment | Frank Hüllmandel | Whatsapp: +49 (0) 177 299 5212 | E-Mail: info@franksberlin.com | <a href=''>Directions</a> | <a href=''>Imprint</a>`},
 }
 
 export const apartmentContent = [
@@ -55,13 +55,13 @@ export const apartmentContent = [
     
     de: {
         name: 'Küche', 
-        description: 'Die Küche mit Spüle, Herd, Geschirrspüler und Kühlschrank ist komplett für Selbstversorger ausgestattet. Die Basics Kaffee,Tee, Kakao und Müsli sind inklusive.', 
+        description: 'Die Küche mit Spüle, Herd, Geschirrspüler und Kühlschrank ist komplett für Selbstversorger ausgestattet. Die Basics Kaffee,Tee, Kakao, Müsli und Gewürze sind inklusiv.', 
         
         images: ['/küche/hc_015.jpg','/küche/hc_016.jpg', '/küche/hc_017.jpg', '/küche/hc_018.jpg'] 
     },
     en: {
         name: 'Kitchen', 
-        description: 'The kitchen, with sink, gas stovetop, electric oven and refrigerator, is completely furnished for self-catering. The basics (coffee, tea, cocoa and muesli) are provided.', 
+        description: 'The kitchen, with sink, gas stovetop, electric oven and refrigerator, is completely furnished for self-catering. The basics coffee, tea, cocoa, muesli and spices are provided.', 
         
         images: ['/küche/hc_015.jpg','/küche/hc_016.jpg', '/küche/hc_017.jpg', '/küche/hc_018.jpg'] ,
     },
@@ -70,13 +70,13 @@ export const apartmentContent = [
     {
         de: {
             name: 'Bad', 
-            description: 'Das moderne Bad ist neu gefliest und mit Dusche und WC ausgestattet. Handtücher liegen für Euch bereit.', 
+            description: 'Das moderne Bad ist neu gefliest und mit Dusche, WC und Waschmaschine ausgestattet. Handtücher liegen für Euch bereit.', 
             images: ['/bad/hc_020.jpg','/bad/hc_021.jpg', '/bad/hc_022.jpg'],
 
         },
         en: {
             name: "Bathroom",
-            description: "The modern bathroom has been recently tiled and is furnished with a shower and toilet. Towels are provided for you.",
+            description: "The modern bathroom is newly tiled and equipped with shower, toilet and washing machine. Towels are provided for you.",
             images: ['/bad/hc_020.jpg','/bad/hc_021.jpg', '/bad/hc_022.jpg'],
         },
     },
@@ -87,16 +87,19 @@ export const ContactContent = {
     de: {
         heading: 'Kontakt',
         adresse: 'Adresse',
-        Telefon: 'Telefon',
-        message: `Für eine Buchungsanfrage schickt uns bitte eine E-Mail an <span>info@franksberlin.com</span> oder ruft uns an <span>Festnetz 030. 91704776 Mobil unter 0177. 2995212</span>`,
-        MobilInfo: `Festnetz: +49 (0)30. 91704776 Mobil: +49 (0)177. 2995212`,
+        Telefon: 'Whatsapp',
+        MobilInfo: `+49 (0) 177 299 5212`,
+        emailHeading: 'Email',
+        email:  'info@franksberlin.com'
+
     },
     en: {
         heading: 'Contact',
         adresse: 'Address',
-        Telefon: 'Telephone',
-        message: `For a booking request please email us at <span>info@franksberlin.com</span> or call us on <span> landline 030. 91704776 mobile on 0177. 2995212</span>`,
-        MobilInfo: "Landline: +49 (0)30. 91704776 Mobile: +49 (0)177. 2995212",
+        Telefon: 'Whatsapp',   
+        MobilInfo: "+49 (0) 177 299 5212",
+        emailHeading: 'Email',
+        email: 'info@franksberlin.com',
 
     }
 }
