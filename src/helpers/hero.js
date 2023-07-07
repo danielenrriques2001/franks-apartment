9c23ecb85b2a510d17d4aa6a93aea938e86ab193
daniel <daniel.enrriquesmercedes@gmail.com>
2023-07-07 17:26:19 +0200

    ft: I fixed the hero

--- src/helpers/hero.js
+++ src/helpers/hero.js
@@ -1,11 +1,5 @@
 let images = [
     '/franks-1.webp', 
-    '/franks-2.webp', 
-    '/franks-3.webp',
-    '/franks-4.webp',
-    '/franks-5.webp',
-    '/franks-6.webp',
-    '/franks-7.webp',
     '/franks-8.webp',
     '/franks-9.webp',
     '/franks-10.webp'
