65d9df921d1d9c5ca516b3f9dd76e943b52f5c7b
daniel <daniel.enrriquesmercedes@gmail.com>
2023-07-27 12:45:42 +0200

    refactor: I`ve changed the hero translation with the UI file

--- src/utils/ui.ts
+++ src/utils/ui.ts
@@ -10,10 +10,19 @@ export const languages = {
     de: {
       'nav.home': 'Home',
       'nav.apartment': 'Apartment',
-      'nav.prenslauerBerg': 'Prenslauer Berg',
+      'nav.prenzlauerBerg': 'Prenzlauer Berg',
       'nav.kontakt': 'Kontakt',
+      'hero': {
+        title: `Dein <span>Zuhause</span> in unserem Kiez!`,
+        button: 'Verfügbarkeit prüfen',
+    
+      }
     },
     en: {
       'nav.kontakt': 'Contact',
+      'hero': {
+        title: `Your <span>Home</span> in our Kiez!`,
+        button: 'Check Availability',
+      }
     },
   } as const;
